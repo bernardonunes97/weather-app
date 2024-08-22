7229b8f0f77ad30d7db2498349bae8716d0c46e2
Bernardo Nunes <bernardonunes@Bernardos-MacBook-Pro.local>
2024-08-22 09:58:11 -0300

    creating search module directory

--- weather-app/weather-app/Search/SearchView.swift
+++ weather-app/weather-app/Search/SearchView.swift
@@ -1,5 +1,5 @@
 //
-//  ContentView.swift
+//  SearchView.swift
 //  weather-app
 //
 //  Created by Bernardo Nunes on 22/08/24.
@@ -7,7 +7,7 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct SearchView: View {
     var body: some View {
         VStack {
             Image(systemName: "globe")
@@ -20,5 +20,5 @@ struct ContentView: View {
 }
 
 #Preview {
-    ContentView()
+    SearchView()
 }
