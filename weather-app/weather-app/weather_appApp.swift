9d32f12fad5203125ec2b3df2d4751922d8630f2
Bernardo Nunes <bernardonunes@Bernardos-MacBook-Pro.local>
2024-08-22 09:41:24 -0300

    creating project

---
+++ weather-app/weather-app/weather_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  weather_appApp.swift
+//  weather-app
+//
+//  Created by Bernardo Nunes on 22/08/24.
+//
+
+import SwiftUI
+
+@main
+struct weather_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
